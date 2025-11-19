a8dbdbaf762778e68869dad549457f8831ccbf4b
Rounak-singh03 <rajputrounak08@gmail.com>
2025-11-19 21:45:32 +0530

    Fix wrong import (remove getLinkStats)

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,16 +1,12 @@
-import express from "express";
-import {
-  createLink,
-  getAllLinks,
-  getLinkStats,
-  deleteLink
-} from "../controllers/linkscontroller.js";
+import { Router } from "express";
+import { createLink, getAllLinks, getLink, deleteLink } 
+from "../controllers/linkscontroller.js";
 
-const router = express.Router();
+const router = Router();
 
 router.post("/", createLink);
 router.get("/", getAllLinks);
-router.get("/:code", getLinkStats);
+router.get("/:code", getLink);
 router.delete("/:code", deleteLink);
 
 export default router;
